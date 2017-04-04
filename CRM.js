0559c9057de3fe74964c7ab4dfab3210ef53e00b
DrewMurr2 <drewmurr2@gmail.com>
2017-04-04 18:11:45 -0500

    go

--- CRM.js
+++ CRM.js
@@ -1,15 +1,15 @@
 var un
 var pw
-
+// SHFT ALT F to format
 function logInOrOut(usr, pwd) {
     un = usr
     pw = pwd
-    call('http://clientconnectcrm.azurewebsites.net/login.php', undefined,function(response){
-        if(response=="Yes"){
+    call('http://clientconnectcrm.azurewebsites.net/login.php', undefined, function (response) {
+        if (response == "Yes") {
             getLeads()
-            $(".classified").css('display','block')
-        }else{
-            $(".classified").css('display','none')
+            $(".classified").css('display', 'block')
+        } else {
+            $(".classified").css('display', 'none')
         }
     })
 }
@@ -26,11 +26,7 @@ function call(url, additional, callback) {
         , type: "post"
         , data: "un=" + un + "&pw=" + pw + adparams
         , success: function (response) {
-                if(callback)callback(response)
-            console.log(response)
-            var t = JSON.parse(response)
-            console.log(t);
-            // you will get response from your php page (what you echo or print)                 
+            if (callback) callback(response)
         }
         , error: function (jqXHR, textStatus, errorThrown) {
             alert(textStatus, errorThrown);
@@ -38,13 +34,89 @@ function call(url, additional, callback) {
     });
 }
 
-function getLeads(){
-call('Leads.php', undefined,function(){
+function getLeads() {
+    call('Leads.php', undefined, function (response) {
+        var data = JSON.parse(response)
+        var companies = data.data[0].company
+        companies.forEach(function (company) {
+            retrieveObject(function (object) {
+                leads.push(new lead(company, object))
+
+            })
 
 
-})
+
+        })
+
+
+
+        companies.forEach(function (company) {
+        })
+    })
+}
+
+var leads = []
+
+function lead(company, obj) {
+    this.name = company.name
+    this.id = company.id
+    if (isString(company.background))
+        this.background = company.background
+
+    if (obj.Phases) this.Phases = obj.Phases
+    else {
+
+        this.Phases = {}
+        this.Phases.list = []
+        this.Phases.Cold = new phase({ name: "Cold", targetDuration: 60 })
+        this.Phases.list.push(this.Phases.Cold)
+        this.Phases.Interested = new phase({ name: "Interested", targetDuration: 14 })
+        this.Phases.list.push(this.Phases.Interested)
+        this.Phases.Demonstrations = new phase({ name: "Demonstrations", targetDuration: 8 })
+        this.Phases.list.push(this.Phases.Demonstrations)
+        this.Phases.TermsAndConditions = new phase({ name: "TermsAndConditions", targetDuration: 4 })
+        this.Phases.list.push(this.Phases.TermsAndConditions)
+        this.Phases.Install = new phase({ name: "Install", targetDuration: 18 })
+        this.Phases.list.push(this.Phases.Install)
+        this.Phases.Training = new phase({ name: "Training", targetDuration: 6 })
+        this.Phases.list.push(this.Phases.Training)
+        this.Phases.PaymentConfirmation = new phase({ name: "PaymentConfirmation", targetDuration: 2 })
+        this.Phases.list.push(this.Phases.PaymentConfirmation)
+    }
+
+    if (obj.Install) this.Phases = obj.Phases
+    else {
+        this.Install = {}
+        this.Install.Phases = {}
+        this.Install.Phases.accessToServer = new phase({ name: "accessToServer", targetDuration: 2 })
+        this.Install.Phases.installQuestionsAnswered = new phase({ name: "installQuestionsAnswered", targetDuration: 2 })
+        this.Install.Phases.configuredFirmSettings = new phase({ name: "configuredFirmSettings", targetDuration: 2 })
+        this.Install.Phases.syncNeedles = new phase({ name: "syncNeedles", targetDuration: 2 })
+        this.Install.Phases.testProduction = new phase({ name: "testProduction", targetDuration: 2 })
+        this.Install.Phases.scheduleTraining = new phase({ name: "scheduleTraining", targetDuration: 2 })
+    }
+
+    if(!obj)this.createObj()
+    
+
+    function phase(options) {
+        this.name = options.name || null,
+            this.start = options.start || null,
+            this.end = options.end || null,
+            this.targetDuration = options.targetDuration || null
+    }
 
 }
+
+
+lead.prototype.createObj = function () {
+
+}
+
+function isString(myVar) {
+    return (typeof myVar === 'string' || myVar instanceof String) || false
+}
+
 
 // function getLeads() {
 //     var xmlhttp = new XMLHttpRequest();
@@ -75,9 +147,11 @@ var highRiseClientConnectCaseObj = {
         , First_training: undefined
         , Received_payment_info: undefined
         , Received_first_payment: undefined
-    , }
+        ,
+    }
     , Demos: []
     , Trainings: []
     , Calls: []
     , Emails: []
-, }
+    ,
+}
