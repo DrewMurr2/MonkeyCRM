e5f3a61c928953fb473f16d282d059a0bf817490
DrewMurr2 <drewmurr2@gmail.com>
2017-04-05 17:25:16 -0500

    Got leads to all import to console

--- CRM.js
+++ CRM.js
@@ -39,17 +39,16 @@ function getLeads() {
         var data = JSON.parse(response)
         var companies = data.data[0].company
         companies.forEach(function (company) {
-            retrieveObject(function (object) {
-                leads.push(new lead(company, object))
-
+            retrieveObject(company, function (objec) {
+                // if (company.name.includes('Martin')|| company.name.includes('Schwartz')|| company.name.includes('Fox')) {
+                //     leads.push(new lead(company))
+                // } else {
+                    obj = ParseJSON(objec)
+                    if (obj.data[0].obj) leads.push(new lead(company, obj.data[0].obj))
+                    else leads.push(new lead(company))
+                //}
             })
-
-
-
         })
-
-
-
         companies.forEach(function (company) {
         })
     })
@@ -63,41 +62,34 @@ function lead(company, obj) {
     if (isString(company.background))
         this.background = company.background
 
-    if (obj.Phases) this.Phases = obj.Phases
+    if (obj && obj.Phases) this.Phases = obj.Phases
     else {
 
-        this.Phases = {}
-        this.Phases.list = []
-        this.Phases.Cold = new phase({ name: "Cold", targetDuration: 60 })
-        this.Phases.list.push(this.Phases.Cold)
-        this.Phases.Interested = new phase({ name: "Interested", targetDuration: 14 })
-        this.Phases.list.push(this.Phases.Interested)
-        this.Phases.Demonstrations = new phase({ name: "Demonstrations", targetDuration: 8 })
-        this.Phases.list.push(this.Phases.Demonstrations)
-        this.Phases.TermsAndConditions = new phase({ name: "TermsAndConditions", targetDuration: 4 })
-        this.Phases.list.push(this.Phases.TermsAndConditions)
-        this.Phases.Install = new phase({ name: "Install", targetDuration: 18 })
-        this.Phases.list.push(this.Phases.Install)
-        this.Phases.Training = new phase({ name: "Training", targetDuration: 6 })
-        this.Phases.list.push(this.Phases.Training)
-        this.Phases.PaymentConfirmation = new phase({ name: "PaymentConfirmation", targetDuration: 2 })
-        this.Phases.list.push(this.Phases.PaymentConfirmation)
+
+        this.Phases = []
+        this.Phases.push(new phase({ name: "Cold", targetDuration: 60 }))
+        this.Phases.push(new phase({ name: "Interested", targetDuration: 14 }))
+        this.Phases.push(new phase({ name: "Demonstrations", targetDuration: 8 }))
+        this.Phases.push(new phase({ name: "TermsAndConditions", targetDuration: 4 }))
+        this.Phases.push(new phase({ name: "Install", targetDuration: 18 }))
+        this.Phases.push(new phase({ name: "Training", targetDuration: 6 }))
+        this.Phases.push(new phase({ name: "PaymentConfirmation", targetDuration: 2 }))
     }
 
-    if (obj.Install) this.Phases = obj.Phases
+    if (obj && obj.Install) this.Install = obj.Install
     else {
         this.Install = {}
-        this.Install.Phases = {}
-        this.Install.Phases.accessToServer = new phase({ name: "accessToServer", targetDuration: 2 })
-        this.Install.Phases.installQuestionsAnswered = new phase({ name: "installQuestionsAnswered", targetDuration: 2 })
-        this.Install.Phases.configuredFirmSettings = new phase({ name: "configuredFirmSettings", targetDuration: 2 })
-        this.Install.Phases.syncNeedles = new phase({ name: "syncNeedles", targetDuration: 2 })
-        this.Install.Phases.testProduction = new phase({ name: "testProduction", targetDuration: 2 })
-        this.Install.Phases.scheduleTraining = new phase({ name: "scheduleTraining", targetDuration: 2 })
+        this.Install.Phases = []
+        this.Install.Phases.push(new phase({ name: "accessToServer", targetDuration: 2 }))
+        this.Install.Phases.push(new phase({ name: "installQuestionsAnswered", targetDuration: 2 }))
+        this.Install.Phases.push(new phase({ name: "configuredFirmSettings", targetDuration: 2 }))
+        this.Install.Phases.push(new phase({ name: "syncNeedles", targetDuration: 2 }))
+        this.Install.Phases.push(new phase({ name: "testProduction", targetDuration: 2 }))
+        this.Install.Phases.push(new phase({ name: "scheduleTraining", targetDuration: 2 }))
     }
 
-    if(!obj)this.createObj()
-    
+    if (!obj) this.createObj()
+
 
     function phase(options) {
         this.name = options.name || null,
@@ -110,48 +102,36 @@ function lead(company, obj) {
 
 
 lead.prototype.createObj = function () {
-
+    console.log(this.name + '     ', StringJSON(this))
+    console.log()
+    call('http://clientconnectcrm.azurewebsites.net/CreateLeadObj.php', { id: this.id, obj: StringJSON(this) }, function (obj) {
+        console.log(ParseJSON(obj))
+    })
 }
 
+function retrieveObject(company, callback) {
+    call('http://clientconnectcrm.azurewebsites.net/leadObj.php', { id: company.id }, function (obj) {
+        callback(obj)
+    })
+}
 function isString(myVar) {
     return (typeof myVar === 'string' || myVar instanceof String) || false
 }
 
-
-// function getLeads() {
-//     var xmlhttp = new XMLHttpRequest();
-//     xmlhttp.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             var JSO = JSON.parse(this.responseText)
-//             console.log(JSO)
-//         }
-//     };
-//     xmlhttp.open("GET", "Leads.php");
-//     xmlhttp.send();
-// }
-var highRiseClientConnectCaseObj = {
-    caseID: 1215744
-    , VisibleBackgroundInfo: undefined
-    , Phases: {
-        Cold: undefined
-        , First_expressed_interest: undefined
-        , First_demo: undefined
-        , Signed_Terms_and_Conditions: undefined
-        , Began_install: undefined
-        , Received_access_to_server: undefined
-        , Received_settings_selections_from_firm: undefined
-        , Configured_firm_settings: undefined
-        , Began_sync: undefined
-        , Sync_complete: undefined
-        , Test_texting_on_production: undefined
-        , First_training: undefined
-        , Received_payment_info: undefined
-        , Received_first_payment: undefined
-        ,
-    }
-    , Demos: []
-    , Trainings: []
-    , Calls: []
-    , Emails: []
-    ,
+function ParseJSON(str) {
+    var newStr = replaceAll(replaceAll(str, '"{', '{'), '}"', '}')
+    return JSON.parse(newStr)
 }
+
+function StringJSON(obj){
+    var str = JSON.stringify(obj)
+    return replaceAll(str,"&", "AND")
+}
+function replaceAll(str, find, replace) {
+    function escapeRegExp(str) {
+        return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+    }
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+
